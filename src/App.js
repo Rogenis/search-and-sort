327a235c23430f8efe57c475e392ce8ee3a8dfa8
Rógenis Silva <rogenis.silva@ipdec.org>
2022-09-27 16:12:25 -0400

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import DATA from './MOCK_DATA.json';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [searchItem, setSearchItem] = useState('');
+  const [sampleData, setSampleData] = useState(DATA);
+
+  useEffect(() => {
+    setSampleData(sampleData);
+  }, [])
+
+  function sortByName(){
+    const sortedData = [...sampleData].sort((a, b) => {
+      return a.product > b.product ? 1 : -1;
+    })
+    console.log(sortedData);
+    setSampleData(sortedData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text"
+        placeholder="Search..." 
+        onChange={event => { setSearchItem(event.target.value)}} />
+        <button>
+          <span onClick={sortByName}>Ordenar por Nome</span>
+        </button>
+      {
+        sampleData.filter((val) => {
+          if(searchItem === "") {
+            return val
+          } else if (val.product.toLowerCase().includes(searchItem.toLowerCase())) {
+            return val
+          }
+        }).map((val, key) => {
+        return (
+          <div className='product' key={key}>
+            <p>{val.product}</p>
+          </div>
+      )})
+    }
     </div>
   );
 }
